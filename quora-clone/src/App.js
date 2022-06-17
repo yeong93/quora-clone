554543db838c652485802ed824c11e071d86150b
sookyeong <sasa023@naver.com>
2022-06-17 16:14:25 +0900

    1. 리덕스 적용 (로그인) 2. firebase 설정

--- quora-clone/src/App.js
+++ quora-clone/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Quora from './Quora';
+import {useDispatch, useSelector} from "react-redux";
+import { selectUser } from './features/userSlice';
+
+import Login from './Login';
 
 function App() {
+  const user = useSelector(selectUser)
+
   return (
     <div className="App">
       <Quora/>
+      {
+        user ? <Quora/> : <Login/>
+      }
+      
     </div>
   );
 }

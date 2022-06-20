4f4ea86ad18f2fb8e50a7f994988dd4ddce434bb
sookyeong <sasa023@naver.com>
2022-06-20 10:49:46 +0900

    1. 로그인페이지 추가 2. 로그인 css등록 3. 초기화면 로그인상태 추가

--- quora-clone/src/App.js
+++ quora-clone/src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.css';
 import Quora from './Quora';
+import Login from './Login';
 import {useDispatch, useSelector} from "react-redux";
 import { selectUser } from './features/userSlice';
 
-import Login from './Login';
 
 function App() {
   const user = useSelector(selectUser)
-
+  
   return (
     <div className="App">
-      <Quora/>
       {
         user ? <Quora/> : <Login/>
       }
--- quora-clone/src/Login.js
+++ quora-clone/src/Login.js
@@ -1,12 +1,96 @@
+import { ArrowForwardIos } from '@material-ui/icons';
 import React from 'react';
 import './Login.css'
 
 function Login() {
   return (
     <div className="login">
-        
+        <div className="login_container">
+            <div className="login_logo">
+                <img src="https://qph.fs.quoracdn.net/main-qimg-d049946241e53481209a8938b70321e0" alt=""/>
+
+            </div>
+            <div className="login_desc">
+                <p> 리액트 깎는 숙영을 시청해주셔서 감사합니다</p>
+                <h3> 리액트 깎는 숙영 올림 </h3>
+            </div>
+
+            <div className="login_auth">
+                <div className="login_authOptions">
+                    <div className="login_authOption">
+                        <img className="login_gooogleAuth"
+                             src="https://media-public.canva.com/MADnBiAubGA/3/screen.svg"
+                             alt=""/>
+                        <p> 구글 아이디로 로그인 </p>
+                    </div>
+                    <div className="login_authOption">
+                        <img className="login_gooogleAuth"
+                             src="https://1000logos.net/wp-content/uploads/2016/11/Facebook-logo-500x350.png"
+                             alt=""/>
+                        <p> 페이스북 아이디로 로그인 </p>
+                    </div>
+
+
+                    <div className="login_authDesc">
+                        <p>
+                          <span style={{color: "blue", cursor: "pointer"}}>이메일로 회원가입</span>
+                            시 본사의 
+                            <p>
+                              <span style={{color: "blue", cursor: "pointer"}}>
+                                개인정보정책{" "}
+                              </span>
+                            </p>과 {" "}
+                            <span style={{color: "blue", cursor: "pointer"}}>
+                              서비스 제공 정책
+                            </span>
+                            에 동의하는 것으로 간주합니다
+                        </p>
+                    </div>
+                </div>
+
+
+                <div className="login_emailPass">
+                    <div className="login_label">
+                        <h4> 로그인 </h4>
+                    </div>
+
+                        <div className="login_inputFields">
+                            <div className="login_inputField">
+                                <input type="text" placeholder="이메일"/>
+                            </div>
+
+                            <div className="login_inputField">
+                                <input type="password" placeholder="비밀번호" />
+                            </div>
+                        </div>
+
+                        <div className="login_forgButt">
+                            <small> 비밀번호 찾기</small>
+                            <button type="submit">로그인</button>
+                        </div>
+
+                    <button>회원가입</button>
+
+                </div>
+            </div>
+
+            <div className="login_lang">
+              <p> 언어 설정 </p>
+              <ArrowForwardIos fontSize="small" />
+            </div>
+
+            <div className="login_footer">
+                <p>About</p>
+                <p>오시는길</p>
+                <p>비지니스문의</p>
+                <p>근성과끈기</p>
+                <p>구독과좋아요!!</p>
+                <p>&copy; 리액트 깎는 숙영</p>
+            </div>
+
+        </div>
     </div>
-  );
+  )
 }
 
 export default Login;
